124ee1f4e84f3a58d5cf14cd1d853657a904a55a
wojiushijiangnan <1750098659@qq.com>
2025-08-08 20:10:40 +0800

    悬停可视化

--- python_repos_visual.py
+++ python_repos_visual.py
@@ -25,6 +25,22 @@ for repo_dict in repo_dicts:
 # 可视化
 title = "Most-Starred Python Projects on GitHub"
 labels = {"x": "Repository", "y": "Stars"}
-fig = px.bar(x = repo_names, y = stars,title=title, labels=labels)
+
+# 添加鼠标提示
+repo_dicts = response_dict["items"]
+repo_names, stars, hover_texts = [], [], []
+for repo_dict in repo_dicts:
+    repo_names.append(repo_dict["name"])
+    stars.append(repo_dict["stargazers_count"])
+
+# 创建悬停文本
+# 提取每个项目的所有者和藐视
+    owner = repo_dict["owner"]["login"]
+    description = repo_dict["description"]
+    # plotly可以使用HTML代码
+    hover_text = [f"{owner}<br />{description}"]
+    hover_texts.append(hover_text)
+#     hover_name参数把悬停给到可视化
+fig = px.bar(x = repo_names, y = stars,title=title, labels=labels,hover_name=hover_texts)
 fig.update_layout(title_font_size=24,xaxis_title_font_size=18,yaxis_title_font_size=16)
 fig.show()
